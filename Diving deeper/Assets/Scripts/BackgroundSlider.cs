deecae818d3ffbbb9265f8b957919a6f7d4c9f6e
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2023-08-25 20:35:42 +0600

    Background sliding

---
+++ Diving deeper/Assets/Scripts/BackgroundSlider.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSlider : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject backgroundPrefab;
+    private Transform backgroundTransform;
+    private Transform instantiatedBGTransform;
+    private Vector3 startingPos;
+    private BoxCollider2D bc;
+    private GameObject instantiatedBG;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        InitiateBackGround();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //backgroundTransform = instantiatedBG.GetComponent<Transform>();
+        if(instantiatedBGTransform.position.y >= startingPos.y)
+        {
+            Destroy(background);
+            InitiateBackGround();
+        }
+    }
+
+    void InitiateBackGround()
+    {
+        background = FindObjectOfType<BackgroundMove>().gameObject;
+        backgroundTransform = background.GetComponent<Transform>();
+        startingPos = backgroundTransform.position;
+        bc = background.GetComponent<BoxCollider2D>();
+        float y = bc.size.y;
+        Vector3 pos = new Vector3(backgroundTransform.position.x, backgroundTransform.position.y - y, backgroundTransform.position.z);
+        instantiatedBG = Instantiate(backgroundPrefab, pos, Quaternion.identity);
+        instantiatedBGTransform = instantiatedBG.transform;
+    }
+}
