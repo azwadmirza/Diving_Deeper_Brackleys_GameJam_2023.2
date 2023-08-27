e3c251ed2757884ae280c288f3b8afe2a1881a68
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-08-27 15:33:42 +0600

    Main Menu

--- Diving deeper/Assets/Scripts/MainMenu.cs
+++ Diving deeper/Assets/Scripts/MainMenu.cs
@@ -9,33 +9,12 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        StartCoroutine(WaitForLoadCompletion());
+        SceneManager.LoadScene("Tileset", LoadSceneMode.Single);
     }
 
     public void ExitGame()
     {
         Debug.Log("Quits Game");
         Application.Quit();
-    }
-
-    private IEnumerator WaitForLoadCompletion()
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync("Tileset", LoadSceneMode.Single);
-        operation.allowSceneActivation = false;
-
-        while (!operation.isDone)
-        {
-            // You can display a loading progress bar or other UI here
-            float progress = Mathf.Clamp01(operation.progress / 0.9f); // Normalize progress (0.9 is the point when the scene is almost loaded)
-            Debug.Log("Loading Progress: " + (progress * 100) + "%");
-
-            // If the scene is almost loaded, activate it to complete the transition
-            if (operation.progress >= 0.9f)
-            {
-                operation.allowSceneActivation = true;
-            }
-
-            yield return null;
-        }
     }
 }
